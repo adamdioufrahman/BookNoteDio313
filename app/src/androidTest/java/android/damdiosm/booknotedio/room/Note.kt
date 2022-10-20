bbbe152ea5fa69b8ef177e8006d513dfafdbd54d
adamdioufrahman <100377308+adamdioufrahman@users.noreply.github.com>
2022-10-20 10:37:31 +0700

    Initial commit

---
+++ app/src/androidTest/java/android/damdiosm/booknotedio/room/Note.kt
@@ -0,0 +1,12 @@
+package android.damdiosm.booknotedio
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class Note(
+    @PrimaryKey(autoGenerate = true)
+    val id : Int = 0,
+    val title: String,
+    val note: String
+)
